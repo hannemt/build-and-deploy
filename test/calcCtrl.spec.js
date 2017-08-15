73eec027add6b407cc343eaa5d53bac570ce4395
Hanne Marie <hanne.trelease@bouvet.no>
2017-08-15 14:56:16 +0200

    subtraction with error

--- test/calcCtrl.spec.js
+++ test/calcCtrl.spec.js
@@ -81,12 +81,12 @@ describe('the calculation controller', function () {
       });
     });
 
-    /*describe('minus operator', function () {
+    describe('minus operator', function () {
       var operator = 'minus';
 
       it('should calculate 2 - 2 = 0', function () {
         var ctrl = createController();
-        ctrl.calculate(2, 2, operator);
+        ctrl.calculate(2, 3, operator);
 
         expect(ctrl.result).toBe(0);
       });
@@ -125,7 +125,7 @@ describe('the calculation controller', function () {
 
         expect(ctrl.result).toBe(10);
       });
-    });*/
+    });
 
     describe('divide operator', function () {
       var operator = 'divide';
